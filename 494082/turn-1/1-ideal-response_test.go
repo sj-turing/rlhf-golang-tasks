b90a25285518440806c1eeaa8fece4817a660c7f
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-01 00:47:58 +0530

    added 1-ideal-response

---
+++ 494082/turn-1/1-ideal-response_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+const (
+	numStrings   = 100000
+	stringLength = 10
+)
+
+// Generate random strings for benchmarking
+func generateStrings() []string {
+	strs := make([]string, numStrings)
+	for i := range strs {
+		bytes := make([]byte, stringLength)
+		for j := 0; j < stringLength; j++ {
+			bytes[j] = byte('a' + rand.Intn(26))
+		}
+		strs[i] = string(bytes)
+	}
+	return strs
+}
+
+// Benchmark fmt.Sprintf
+func BenchmarkSprintf(b *testing.B) {
+	strs := generateStrings()
+	for n := 0; n < b.N; n++ {
+		var result string
+		for _, str := range strs {
+			result = fmt.Sprintf("%s %s", result, str)
+		}
+	}
+}
+
+// Benchmark string.Concat
+func BenchmarkConcat(b *testing.B) {
+	strs := generateStrings()
+	for n := 0; n < b.N; n++ {
+		var result string
+		for _, str := range strs {
+			result += str
+		}
+	}
+}
