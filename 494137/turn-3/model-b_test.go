9f843a86b336eb68e1bf5bb429ab6e37218f78c2
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-04 01:59:44 +0530

    added 3b

--- 494137/turn-3/model-b_test.go
+++ 494137/turn-3/model-b_test.go
@@ -34,44 +34,6 @@ func BenchmarkCountWords(b *testing.B) {
 	}
 }
 
-// Test using map[string]int
-func BenchmarkCountWordsStringInt(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = countWordsStringInt("testdata/words.txt")
-	}
-}
-
-// Test using map[string]uint64
-func BenchmarkCountWordsStringUint64(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = countWordsStringUint64("testdata/words.txt")
-	}
-}
-
-// Test using map[interface{}]int
-func BenchmarkCountWordsInterfaceInt(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = countWordsInterfaceInt("testdata/words.txt")
-	}
-}
-
-// Implementation of countWords using map[string]int
-func countWordsStringInt(fileName string) (map[string]int, error) {
-	// ... (Same code as before)
-}
-
-// Implementation of countWords using map[string]uint64
-func countWordsStringUint64(fileName string) (map[string]uint64, error) {
-	wordCount := make(map[string]uint64)
-	// ... (Same code as before, but increment count using uint64)
-}
-
-// Implementation of countWords using map[interface{}]int
-func countWordsInterfaceInt(fileName string) (map[interface{}]int, error) {
-	wordCount := make(map[interface{}]int)
-	// ... (Same code as before, but use interface{} as key)
-}
-
 func main() {
 	wordCount, err := countWords("testdata/words.txt")
 	if err != nil {
