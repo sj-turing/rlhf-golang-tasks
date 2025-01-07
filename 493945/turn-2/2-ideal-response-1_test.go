dd2515ab67d0b4fe9fb3c6d71516fd7b0e5c888c
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-07 12:00:37 +0530

    added 2-ideal-response

---
+++ 493945/turn-2/2-ideal-response-1_test.go
@@ -0,0 +1,21 @@
+// filename: recursive_factorial_test.go
+package main
+
+import (
+	"testing"
+)
+
+// Factorial calculates the factorial of a number using recursion
+func Factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * Factorial(n-1)
+}
+
+// BenchmarkFactorial benchmarks the recursive factorial function
+func BenchmarkFactorial(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Factorial(10) // Use a large number for significant benchmarking
+	}
+}
---
+++ 493945/turn-2/2-ideal-response-2_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+// IterativeFactorial calculates the factorial of a number using iteration
+func IterativeFactorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	result := 1
+	for i := 2; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
+
+// BenchmarkIterativeFactorial benchmarks the iterative factorial function
+func BenchmarkIterativeFactorial(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IterativeFactorial(10) // Use a large number for significant benchmarking
+	}
+}
