aebe2ba8f4180331e0e4696560c4ec380b3530ec
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-13 01:38:19 +0530

    added 1b and 1-ideal-response

---
+++ 512490/turn-1/1-ideal-response.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	numStrings  = 100000
+	concurrency = 100
+)
+
+func main() {
+	// Sequential concatenation
+	start := time.Now()
+	sequentialConcatenation()
+	elapsed := time.Since(start)
+	fmt.Printf("Sequential concatenation took: %s\n", elapsed)
+
+	// Concurrent concatenation
+	start = time.Now()
+	concurrentConcatenation()
+	elapsed = time.Since(start)
+	fmt.Printf("Concurrent concatenation took: %s\n", elapsed)
+
+	// Concurrent concatenation with channels
+	start = time.Now()
+	concurrentConcatenationWithChannels()
+	elapsed = time.Since(start)
+	fmt.Printf("Concurrent concatenation with channels took: %s\n", elapsed)
+}
+
+func sequentialConcatenation() {
+	var result string
+	for i := 0; i < numStrings; i++ {
+		result += fmt.Sprintf("%d", i)
+	}
+	fmt.Println(len(result))
+}
+
+func concurrentConcatenation() {
+	var result string
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	for i := 0; i < concurrency; i++ {
+		go func(start, end int) {
+			defer wg.Done()
+			for i := start; i < end; i++ {
+				result += fmt.Sprintf("%d", i)
+			}
+		}(i*numStrings/concurrency, (i+1)*numStrings/concurrency)
+	}
+
+	wg.Wait()
+	fmt.Println(len(result))
+}
+
+func concurrentConcatenationWithChannels() {
+	var result string
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	ch := make(chan string)
+
+	for i := 0; i < concurrency; i++ {
+		go func(start, end int) {
+			defer wg.Done()
+			var part string
+			for i := start; i < end; i++ {
+				part += fmt.Sprintf("%d", i)
+			}
+			ch <- part
+		}(i*numStrings/concurrency, (i+1)*numStrings/concurrency)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for part := range ch {
+		result += part
+	}
+	fmt.Println(len(result))
+}
