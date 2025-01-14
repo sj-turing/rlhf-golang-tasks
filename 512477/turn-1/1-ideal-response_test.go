082995eba6a95ec860ce83fe0cce9a8a0f2abf55
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-14 14:52:15 +0530

    added 1-ideal-response

---
+++ 512477/turn-1/1-ideal-response_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+const (
+	alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
+)
+
+var inputString = generateStrings()
+
+// Generate a slice of 100 strings for benchmarking
+func generateStrings() []string {
+	result := make([]string, 100)
+	for index := range result {
+		result[index] = getString(rand.Intn(50))
+	}
+	return result
+}
+
+func getString(length int) string {
+	str := ""
+
+	for length > 0 {
+		str += string(alphanum[rand.Intn(len(alphanum))])
+		length--
+	}
+	return str
+}
+
+func concatBySprintf(strings []string) string {
+	result := ""
+	for _, str := range strings {
+		result = fmt.Sprintf("%s%s", result, str)
+	}
+	return result
+}
+
+func concatByBuffer(strings []string) string {
+	var buf = &bytes.Buffer{}
+	for _, s := range strings {
+		buf.WriteString(s)
+	}
+	return buf.String()
+}
+
+func concatByPlusOperator(strings []string) string {
+	result := ""
+	for _, str := range strings {
+		result += str
+	}
+	return result
+}
+
+// Benchmark using fmt.Sprintf
+func BenchmarkConcatBySprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatBySprintf(inputString)
+	}
+}
+
+// Benchmark using traditional string concatenation
+func BenchmarkConcatenation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatByPlusOperator(inputString)
+	}
+}
+
+// Benchmark using bytes buffer for optimized concatenation
+func BenchmarkBuilder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatByBuffer(inputString)
+	}
+}
