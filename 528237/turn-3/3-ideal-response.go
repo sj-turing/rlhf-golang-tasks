db432fe65363500bfbda24a8641d0618dad5bd5d
Shubham Jagdhane <jagdhane.s@turing.com>
2025-01-21 01:11:32 +0530

    added 3-ideal-response.go

---
+++ 528237/turn-3/3-ideal-response.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	_ "net/http/pprof"
+	"strconv"
+	"sync"
+)
+
+type User struct {
+	Username string
+	Email    string
+	Age      int
+}
+
+var (
+	userPool = sync.Pool{
+		New: func() interface{} {
+			return new(User)
+		},
+	}
+)
+
+func GetUserDetails(w http.ResponseWriter, r *http.Request) {
+	numUsers, err := strconv.Atoi(r.URL.Query().Get("num"))
+	if err != nil || numUsers <= 0 {
+		http.Error(w, "Invalid number of users", http.StatusBadRequest)
+		return
+	}
+
+	users := make([]*User, 0, numUsers)
+	for i := 0; i < numUsers; i++ {
+		user := userPool.Get().(*User)
+		user.Username = "User" + strconv.Itoa(i)
+		user.Email = "user" + strconv.Itoa(i) + "@example.com"
+		user.Age = i % 100
+		users = append(users, user)
+	}
+
+	response, _ := json.Marshal(users)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+
+	// Return users to pool
+	for _, user := range users {
+		userPool.Put(user)
+	}
+}
+
+func main() {
+	http.HandleFunc("/userdetails", GetUserDetails)
+	http.ListenAndServe(":8080", nil)
+}
